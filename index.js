d817876e1aaf2dc8a8bd93fcd7874fa3d1839023
taufiq72 <taufiqq1207@gmail.com>
2026-08-19 11:43:07 +0700

    debug lagi

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const connectDatabase = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
