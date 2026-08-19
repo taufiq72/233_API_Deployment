90cd10993a1053bcbf4d9eb44220712b6cbac94d
taufiq72 <taufiqq1207@gmail.com>
2026-08-19 12:23:07 +0700

    Fix

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const penulisController = require('../controller/penulisController');
+const penulisController = require("../controller/penulisController");
 const komikController = require('../controller/komikController');
 const genreController = require('../controller/genreController');
 const authMiddleware = require('../middleware/authMiddleware');
